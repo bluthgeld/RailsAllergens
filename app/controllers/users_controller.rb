0ef290481ed77ded365f616c2ffd432a18ad1189
Robert Pedersen <rob@worldcrimeleague.org>
2019-06-13 14:20:04 -0400

    recipes controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
 
-  def show
-
-    @user = User.find(params[:id])
-
+  def index
+    @users = User.all
   end
 
-  
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
 
 end
